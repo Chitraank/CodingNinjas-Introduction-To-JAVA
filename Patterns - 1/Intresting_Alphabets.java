366c15b30475a1d620cfe68e63c448110e8b70f8
Chitraank Singh <45902220+Chitraank@users.noreply.github.com>
2020-06-05 07:43:19 +0530

    Code: Interesting_Alphabets
    Print the following pattern for the given number of rows.
    Pattern for N = 5
    E
    DE
    CDE
    BCDE
    ABCDE
    Input format :
    N (Total no. of rows)
    Output format :
    Pattern in N lines
    Constraints
    0 <= N <= 26
    Sample Input 1:
    8
    Sample Output 1:
    H
    GH
    FGH
    EFGH
    DEFGH
    CDEFGH
    BCDEFGH
    ABCDEFGH
    Sample Input 2:
    7
    Sample Output 2:
    G
    FG
    EFG
    DEFG
    CDEFG
    BCDEFG
    ABCDEFG

---
+++ Patterns - 1/Intresting_Alphabets.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+        	char ch =(char)('A'+n-i);
+            for(int j=1;j<=i;j++)
+            {
+                
+                System.out.print(ch);     
+                ch=(char)(ch+1);
+            }
+            System.out.println();
+           
+        }
+	
+	}
+}
