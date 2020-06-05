d44ae509518c63a0235e9fd924da5351d98e24f8
Chitraank Singh <45902220+Chitraank@users.noreply.github.com>
2020-06-05 07:30:00 +0530

    Print the following pattern
    Pattern for N = 4
    4444
    4444
    4444
    4444
    Input format :
    Integer N (Total no. of rows)
    Output format :
    Pattern in N lines
    Constraints
    0 <= N <= 50
    Sample Input 1:
    7
    Sample Output 1:
    7777777
    7777777
    7777777
    7777777
    7777777
    7777777
    7777777
    Sample Input 1:
    6
    Sample Output 1:
    666666
    666666
    666666
    666666
    666666
    666666

---
+++ Patterns - 1/Square_Pattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+	Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+		
+	}
+
+}
